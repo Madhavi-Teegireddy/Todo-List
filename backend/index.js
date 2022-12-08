a150012af6a15afa7129b850c1bcbc644590d400
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-08 12:06:45 +0530

    server-mongo

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 
 import express from 'express';
-
+import Connection from './database/db.js';
 
 const app = express();
 
-
 const PORT = 8080;
+
+Connection();
 
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
